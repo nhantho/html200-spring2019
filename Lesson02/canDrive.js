3eb2ab24268456848d61d380cfe2327c89ca201f
nhantho <natalie.crippen@gmail.com>
2019-04-14 15:37:28 -0700

    programed canDrive

---
+++ Lesson02/canDrive.js
@@ -0,0 +1,13 @@
+const firstName= "John"
+const lastName= "Smith"
+var age= 21
+var x= 16
+var y= 6
+
+if (age >= x) {
+  console.log(firstName+' '+lastName+' can drive and has been driving for '+(age-x)+' years');
+} else {
+  while (y > 0) {
+  console.log(y-1);
+  y--; }
+}
