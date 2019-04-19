7d4cf28bd16d0cc62b413f4de5558688734c24fc
nhantho <natalie.crippen@gmail.com>
2019-04-18 19:17:11 -0700

    added and updated Exercise 2

---
+++ Lesson03/exercise-two.js
@@ -0,0 +1,7 @@
+let haroldWilson = 'I\'m an optimist, but an optimist who carries a raincoat.';
+
+// create an array, where each word is an element
+let words = haroldWilson.split(' ');
+
+// log the 3rd word
+console.log(words[2]);
