e803972208fe9b2e5a9858d9627a9379a778258b
nhantho <natalie.crippen@gmail.com>
2019-04-24 18:35:47 -0700

    completed assignment and extra challenge

---
+++ Lesson04/Assignment01/script.js
@@ -0,0 +1,18 @@
+function squareInput() {
+  let input = prompt('Enter a number to square');
+  let result = 0;
+
+  result = input * input;
+
+  alert(result);
+}
+
+function multiplyNumber() {
+  let inputOne = prompt('Enter your first number');
+  let inputTwo = prompt('Enter your second number');
+  let result = 0;
+
+  result = inputOne * inputTwo;
+
+  alert(result);
+}
